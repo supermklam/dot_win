debdc46d597739ac494d269439909893385b4529
supermklam <supermklam@yahoo.com.hk>
2020-02-14 14:16:12 +0800

    git

---
+++ win_org.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dot_windows
+{
+    static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            MessageBox.Show("the program started");
+            Application.Run(new Form1());
+        
+        }
+    }
+}
