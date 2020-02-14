949bf38fd56a590850feb1d21eff47ffadbaa95e
supermklam <supermklam@yahoo.com.hk>
2020-02-15 00:43:03 +0800

    Change class program name

--- win_prg.cs
+++ win_prg.cs
@@ -6,7 +6,7 @@ using System.Windows.Forms;
 
 namespace dot_windows
 {
-    static class Program
+    static class win_prg
     {
         /// <summary>
         ///  The main entry point for the application.
